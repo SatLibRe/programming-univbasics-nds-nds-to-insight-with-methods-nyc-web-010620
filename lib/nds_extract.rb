4ef72dbbc893733d934d02f37f9e628b596c482b
Miles Higbie <miles.higbie@gmail.com>
2019-12-17 02:33:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,28 +7,26 @@ require 'directors_database'
 
 require "pry"
 
-def directors_totals(nds)
-  result = {}
-  director_index = 0
-  while director_index < nds.size do
-    director = nds[director_index]
-    result[director[:name]] = gross_for_director(director)
-    director_index += 1
-  end
-  result
-end
+# def directors_totals(nds)
+#   result = {}
+#   director_index = 0
+#   while director_index < nds.size do
+#     director = nds[director_index]
+#     result[director[:name]] = gross_for_director(director)
+#     director_index += 1
+#   end
+#   result
+# end
 
-def gross_for_director(hash)
-  total = 0
-  index = 0
-
-  while index < hash[:movies].length do
-    total += hash[:movies][index][:worldwide_gross]
-    index += 1
-  end
-
-  total
-end
+# def gross_for_director(hash)
+#   total = 0
+#   index = 0
+#   while index < hash[:movies].length do
+#     total += hash[:movies][index][:worldwide_gross]
+#     index += 1
+#   end
+#   total
+# end
 
 
 # def gross_for_director(nds)
